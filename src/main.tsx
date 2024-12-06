33b3579d03fc23ca2dbda60965075d9a3d9f930f
godzilla-s <472214158@qq.com>
2024-12-06 16:40:38 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.tsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://godzilla-s.github.io/mytwa/tonconnect-manifest.json';
 
 createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
